545ed35db33b24ebf3a99c6cc5d506da9e9f8d55
yattinda <yattinda@gmail.com>
2020-11-19 17:01:45 +0900

    Delete seacretkey

--- django_CORGI/CORGI/CORGI/settings.py
+++ django_CORGI/CORGI/CORGI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gntnrp^j3#jr8bue8b#ho793li9zvtse*yis4sl6fez62354&p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
